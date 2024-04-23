c7538845678e78f4162cf8bbe5b0bd94f93587d3
barbara bouillicot <barbara.bouillicot@hotmail.fr>
2024-04-23 08:21:32 +0200

    playlist model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_23_061918) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_23_062103) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "playlists", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_playlists_on_user_id"
+  end
 
   create_table "selected_shows", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -44,6 +52,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_23_061918) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "playlists", "users"
   add_foreign_key "selected_shows", "shows"
   add_foreign_key "selected_shows", "users"
 end
